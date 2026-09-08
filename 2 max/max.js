3cdc605ea07a3c350504eaef7b40a81f49467bee
Aymane <aymanesalmoune00@gmail.com>
2026-09-08 13:20:19 +0100

    added a non function view to the max.js

--- 2 max/max.js
+++ 2 max/max.js
@@ -1,3 +1,4 @@
+//function based
 function Max(arr) {
 let max = arr[0];
 for (let i = 1; i < arr.length; i++) {
@@ -11,3 +12,13 @@ return max;
 let arr1 = [1, 2, 3, 4, 14, 6, 7, 8, 9, 10];
 
 console.log(Max(arr1)); 
+
+// straight code
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let max2 = arr1[0];
+for (let i = 1; i < arr1.length; i++) {
+    if (arr1[i] > max2) {
+        max2 = arr1[i];
+    }
+}
+console.log(max);
