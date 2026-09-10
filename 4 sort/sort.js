0efc90321bf5f2b5c6788747e7337cdf0264756c
Aymane <aymanesalmoune00@gmail.com>
2026-09-10 13:24:13 +0100

    sort algo

--- 4 sort/sort.js
+++ 4 sort/sort.js
@@ -1,22 +1,20 @@
 
-// Method 1 check max or min in an array then remove it from the original array and push it to a new sorted .
+//  loop in arr chaning the the 2 index close to eachother if they need 2 
 
-function Max(arr) {
-let max = arr[0];
-for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-        max = arr[i];
+function loopMethode(arr){
+    let loop = true;
+    let temp = 0;
+    while (loop){
+        loop = false;
+        for (let i=0;i<(arr.length-1);i++){
+            if(arr[i]>arr[i+1]){
+                loop = true;
+                temp = arr[i];
+                arr[i]= arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
     }
+    return arr;
 }
-return max;
-}
-
-function sortMax(unsorted){
-    let sorted = [];
-    let maxV = unsorted[0];
-    while (unsorted.length()>0){
-        maxV =Max(unsorted);
-        sorted.push(maxV);
-    }
-
-}
+console.log(loopMethode(list));
