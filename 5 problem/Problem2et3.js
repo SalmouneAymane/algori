95c0ffd7a525bee981abe42d8ed735a8432018b5
Aymane <aymanesalmoune00@gmail.com>
2026-09-11 12:41:43 +0100

    added ex 3

--- 5 problem/Problem2et3.js
+++ 5 problem/Problem2et3.js
@@ -1,18 +1,10 @@
-let products = [
-    ["a", 30, 1200],
-    ["b", 20, 2500],
-    ["c", 15, 1800],
-    ["d", 40, 3000],
-    ["e", 10, 900],
-    ["f", 25, 2000]
-];
-budget = 100;
 
 
-function popularity (arr){
+
+function popularity (arr,ppltyCondition){
     let popular_products=[];
     for (let i = 0 ;i<arr.length;i++){
-        if (arr[i][2]>1500){
+        if (arr[i][2]>ppltyCondition){
             popular_products[popular_products.length]=arr[i]
             
         }
@@ -54,13 +46,47 @@ function under_Budget(budget,products){
     return inBudget;
 }
 
+// products ex 2
 
+let products = [
+    ["a", 30, 1200],
+    ["b", 20, 2500],
+    ["c", 15, 1800],
+    ["d", 40, 3000],
+    ["e", 10, 900],
+    ["f", 25, 2000]
+];
+let budget = 100;
+let popularityCondition= 1500;
 
-popular_products = popularity(products);
+popular_products = popularity(products,popularityCondition);
 sorted_products = sort_by_price(popular_products);
 inBudget = under_Budget(budget,sorted_products);
 
-
-
 console.log(`popular products :` ,sorted_products);
-console.log(`you can buy: `,inBudget);
+console.log(`you can buy: `,inBudget,` items :`,inBudget.length);
+
+/// videos  ex 3
+
+
+let videos = [
+    ["a", 5, 1200],
+    ["b", 2, 2500],
+    ["c", 1, 1800],
+    ["d", 3, 3000],
+    ["e", 12, 2000],
+    ["f", 3, 2000]
+];
+let time = 10;
+let popularityVideo= 2000;
+
+popular_videos = popularity(videos,popularityVideo);
+sorted_videos = sort_by_price(popular_videos);
+inTime = under_Budget(time,sorted_videos);
+
+console.log(`popular videos :` ,sorted_videos);
+console.log(`you can watch: `,inTime,` Amount :`,inTime.length);
+
+
+
+
